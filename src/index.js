158b2cc812a38a0e698e6043997f3fe21b31c6e8
TATJAVA_Andrei_Tsylko <mi113r@mail.ru>
2018-03-13 22:07:57 +0300

    solved
    solved

--- src/index.js
+++ src/index.js
@@ -1,18 +1,86 @@
 // DO WHATEVER YOU WANT HERE
 
-const createEnumerableProperty = () => {};
-const createNotEnumerableProperty = () => {};
-const createProtoMagicObject = () => {};
-const incrementor = () => {};
-const asyncIncrementor = () => {};
-const createIncrementer = () => {};
+const createEnumerableProperty = (propertyName) => {
+    {
+        propertyName:''
+    }
+};
+const createNotEnumerableProperty = (propertyName) => {
+    Object.defineProperty(Object.prototype, propertyName, {
+        enumerable: false,
+        set: function (newValue) {
+            propertyName = newValue
+        },
+        get: function () {
+            return propertyName;
+        }
+    });
+    return propertyName
+};
+
+const createProtoMagicObject = () => {
+    let object = function a() {
+    };
+    object.__proto__ = object.prototype;
+    return object
+};
+
+
+var value = [];
+const incrementor = () => {
+    value.push(1);
+    return incrementor;
+};
+incrementor.valueOf = function () {
+    return value.length;
+};
+
+var valueAsync = [];
+const asyncIncrementor = () => {
+    return new Promise(resolve =>{
+        valueAsync.push(1);
+        return resolve(valueAsync.length)
+    });
+
+};
+
+
+
+const createIncrementer = () => {
+};
 
 // return same argument not earlier than in one second, and not later, than in two
-const returnBackInSecond = () => {};
-const getDeepPropertiesCount = () => {};
-const createSerializedObject = () => {};
-const toBuffer = () => {};
-const sortByProto = () => {};
+const returnBackInSecond = (value) => {
+   return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(value);
+        }, 1000);
+
+    })
+};
+
+var counter = function (prop) {
+    var count = 0;
+    for(var i in prop) {
+        if(this.hasOwnProperty(i))
+            count = count + 1;
+    }
+    return count;
+};
+
+const getDeepPropertiesCount = (prop) => {
+};
+
+const createSerializedObject = () => {
+    var res = new Boolean(true);
+    return res
+};
+const toBuffer = () => {
+};
+const sortByProto = (value) => {
+    return value.sort((a, b)  => a.__proto__ - b.__proto__)
+};
+
 
 exports.createEnumerableProperty = createEnumerableProperty;
 exports.createNotEnumerableProperty = createNotEnumerableProperty;
